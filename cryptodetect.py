7fecad1fe0a5d894fe70be86ce9082b1b8c06755
Ujjwal Kumar <143947231+ujjwalkumar-64@users.noreply.github.com>
2025-03-31 03:04:11 +0530

    Add files via upload
    first commit

---
+++ cryptodetect.py
@@ -0,0 +1,30 @@
+import pyshark
+from pyad import adimage
+
+def detect_cryptomining_traffic(pcap_file):
+    cap=pyshark.FileCapture(pcap_file)
+    suspicious_ips=[]
+
+    for packet in cap:
+        try:
+            if 'http' in packet:
+                if any(keyword in packet.http.request_full_uri for keyword in ['mining',"cryptominer"]):
+                    suspicious_ips.append(packet.ip.dst)
+        except AttributeError:
+            continue
+
+        return suspicious_ips
+    
+    def extract_cryptomining_evidence(image_file):
+        evidence=[]
+        img= adimage.Adiso(image_file)
+        for entry in img.entries:
+            if any (keyword in entry.name.lower() for keyword in ["mining","cryptominer"]):
+                evidence.append(entry.name)
+
+        return evidence
+    
+    sudpicious_ips =detect_cryptomining_traffic("wiresharkReport.pcap")
+    print("suspicious ips:",suspicious_ips)
+    evidence = extract_cryptomining_evidence("edgeReport.ad1")
+    print("evidence of cryptomining", evidence)
